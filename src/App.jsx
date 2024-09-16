9b2cb9ebf8abac78c0185293bd73dd06fc4b7f98
lchyun23 <lchyun23@gmail.com>
2024-09-17 05:38:53 +0900

    feat: counter 앱 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import '@/App.css'
+import { useState } from 'react'
+import Viewer from '@/components/Viewer'
+import Controller from '@/components/Controller'
 
 function App() {
-  return <div className='App'></div>
+  const [count, setCount] = useState(0)
+  const handleSetCount = (value) => {
+    setCount(count + value)
+  }
+
+  return (
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller handleSetCount={handleSetCount} />
+      </section>
+    </div>
+  )
 }
 export default App

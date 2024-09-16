6eac781394cef903c080250449be37e1b3ec0cd8
lchyun23 <lchyun23@gmail.com>
2024-09-16 20:11:12 +0900

    design: shadcn 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { form } from '@/components/ui/form'
+
 import './App.css'
 import Header from '@/components/Header.jsx'
 import Body from '@/components/Body.jsx'
 import Footer from '@/components/Footer.jsx'
 
 function App() {
-  const name = '보노보노'
+  const name = '천룡인'
+  const location = '숲속마을'
+
   return (
     <div className='App'>
       <Header />
-      <Body name={name} />
+      <Body name={name} location={location} />
+      <Input />
+      <form />
       <Footer />
     </div>
   )
 }
-
 export default App
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,11 @@
-// import '@/components/Body.css'
-// import { PlayCircleIcon } from 'lucide-react'
-import PropTypes from 'prop-types'
+import '@/components/Body.css'
 
-function Body(props) {
-  console.log(props)
-  return <div className='body'>{props.name}는 숲속마을에 거주합니다.</div>
+function Body({ name, location }) {
+  console.log(name, location)
+  return (
+    <div className='body'>
+      {name}은 {location}에 거주합니다.
+    </div>
+  )
 }
-
-// props 타입 검증 설정
-Body.propTypes = {
-  name: PropTypes.string.isRequired, // name은 반드시 문자열이어야 함
-}
-
 export default Body

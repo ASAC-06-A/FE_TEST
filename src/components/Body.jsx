c14c90f5b7aaf7533a241ce5e2f1077635e8bb24
lchyun23 <lchyun23@gmail.com>
2024-09-17 04:07:22 +0900

    feat: State-dropdown 사용하기

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 
 function Body() {
-  const [date, setDate] = useState('')
-  const handelOnChange = (e) => {
-    console.log('변경된 값', e.target.value)
-    setData(e.target.value)
+  const [option, setOption] = useState('')
+  const handleOnChange = (e) => {
+    console.log('변경된 값 : ', e.target.value)
+    setOption(e.target.value)
   }
-
   return (
     <div>
-      <input type='date' value={date} onChange={handelOnChange} />
+      <select value={option} onChange={handleOnChange}>
+        <option key={'1번'}>1번</option>
+        <option key={'2번'}>2번</option>
+        <option key={'3번'}>3번</option>
+      </select>
     </div>
   )
 }

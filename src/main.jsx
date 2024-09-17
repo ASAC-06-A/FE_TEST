a4880ad0b6820db2bc82029dda3e011497b1dc93
lchyun23 <lchyun23@gmail.com>
2024-09-17 21:10:13 +0900

    feat: main.jsx Router-Dom 기능 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,30 @@
 //import { BrowserRouter } from 'react-router-dom';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
 // import { useForm } from 'react-hook-form'
-import App from './App.jsx'
+// import App from './App.jsx'
+// import './index.css'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import * as React from 'react'
+import * as ReactDom from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+])
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <React.StrictMdoe>
+    <RouterProvider router={router} />
+  </React.StrictMdoe>,
 )

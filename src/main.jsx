ec6f270e9a084fd2500f626fe2e02a300e3a576c
lchyun23 <lchyun23@gmail.com>
2024-09-22 01:16:40 +0900

    remove: 파일 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,11 @@
-//import { BrowserRouter } from 'react-router-dom';
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { useForm } from 'react-hook-form'
-// import App from './App.jsx'
-// import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-import * as React from 'react'
-import * as ReactDom from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from 'App'
 import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>Hello world!</div>,
-  },
-])
-
-ReactDom.createRoot(document.getElementById('root')).render(
-  <React.StrictMdoe>
-    <RouterProvider router={router} />
-  </React.StrictMdoe>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )

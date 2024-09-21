d13135831a30d2ac4550a3c892475a112f092d4b
lchyun23 <lchyun23@gmail.com>
2024-09-22 01:03:33 +0900

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,33 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import ErrorPage from '@/error-page'
+import Contact from '@/routes/contact'
+import Root, { loader as rootLoader, action as rootAction } from '@/routes/root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />,
+      },
+    ],
+  },
+])
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
+
 //import { BrowserRouter } from 'react-router-dom';
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
@@ -10,21 +40,3 @@
 //     <App />
 //   </StrictMode>,
 // )
-
-import * as React from 'react'
-import * as ReactDom from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>Hello world!</div>,
-  },
-])
-
-ReactDom.createRoot(document.getElementById('root')).render(
-  <React.StrictMdoe>
-    <RouterProvider router={router} />
-  </React.StrictMdoe>,
-)

f129bf5f7c7413541b24f69877e4e20d185fa307
lchyun23 <lchyun23@gmail.com>
2024-09-23 00:10:27 +0900

    feat: TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
 import 'App.css'
 
-export default function Avatar() {
-  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg'
-  const description = 'Gregorio Y. Zara'
-
-  return <img className='avatar' src={avatar} alt={description} />
+const person = {
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink',
+  },
 }
+
+export default function TodoList() {
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img className='avatar' src='https://i.imgur.com/7vQD0fPs.jpg' alt='Gregorio Y. Zara' />
+
+      <ul
+        style={{
+          backgroundColor: 'black',
+          color: 'yellow',
+        }}
+      >
+        <li>Imporove the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fulled engine</li>
+      </ul>
+    </div>
+  )
+}
+
+// const today = new Date()
+
+// function formatDate(date) {
+//   return new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date)
+// }
+
+// export default function Avatar() {
+//   const avatar = 'https://i.imgur.com/7vQD0fPs.jpg'
+//   const description = 'Gregorio Y. Zara'
+
+//   return <img className='avatar' src={avatar} alt={description} />
+// }
